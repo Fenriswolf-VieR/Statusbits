c3db6032c7e789e8f7c3f4ee403c92708712bb9f
Lucas Walkner <Lucas.Walkner@copadata.com>
2021-07-22 11:45:57 +0200

    initial commit.

---
+++ StatusbitsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statusbits
+{
+    class StatusbitsController
+    {
+        StatusbitsModel model;
+        StatusbitsView view;
+
+        public StatusbitsController(StatusbitsModel model, StatusbitsView view)
+        {
+            this.model = model;
+            this.view = view;
+        }
+
+    }
+}
